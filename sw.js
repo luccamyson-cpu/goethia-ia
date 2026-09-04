54f0911cc46d495cf6be6fad493964ca92861aaf
luccamyson-cpu <luccamyson@gmail.com>
2026-09-04 10:20:10 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='goethia-ia-v14';
+const CACHE='goethia-ia-v18';
 const ASSETS=['./','./index.html','./data/spirits.json','./assets/hero-goethia.jpg','./assets/spirit-placeholder.jpg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
